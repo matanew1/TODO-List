b12ca440055136c36879484309a378dfe8940cbd
matanew1 <matan.bardugo@s.afeka.ac.il>
2023-05-26 11:42:39 +0300

    Revert "rename test 1"
    This reverts commit 75425a26d5d519b2cbbe4b9d1e114272f8662937.

--- server/test/userController.test.js
+++ server/test/userController.test.js
@@ -6,17 +6,32 @@ import { beforeEach } from 'mocha';
 const { expect } = chai;
 
 chai.use(chaiHttp);
-describe('TEST 1 -> CREATE USER AND SEE ALL CURRENT USERSS', () => {
+describe('User Controller', () => {
+//   beforeEach((done) => {
+
+//     chai.request(app)
+//       .delete('/users')
+//       .end((err, res) => {
+//         if (err) {
+//           console.error(err);
+//           done(err);
+//         } else {
+//           done();
+//         }
+//       });
+//   });
+
   it('POST /users -> SHOULD RETURN 201', (done) => {
     chai.request(app)
-      .post('/users')
-      .send(parseJSONFromFile('newUser.json'))
-      .end((err, res) => {
-        expect(res).to.have.status(201);
-        expect(res.body).to.be.not.null;
-        done();
-      });
-  });
+    .post('/users')
+    .send(parseJSONFromFile('newUser.json'))
+    .end((err, res) => {
+      expect(res).to.have.status(201);
+      expect(res.body).to.be.not.null;
+      done();
+    })
+  })
+
   it('GET /users -> SHOULD RETURN 200', (done) => {
     chai.request(app)
       .get('/users')
