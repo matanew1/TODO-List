c936d41c0b18dadd006ddcafd6db3b3ad014a86b
Tamir <tamir.spilberg@s.afeka.ac.il>
2023-06-01 12:17:29 +0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './components/pages/Home.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-up" element={<SignUpPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home/profile/prof/search" element={<Professionals />} />
+                <Route path="/home/profile/prof/search/result" element={<SearchResultProf />} />
+              </Routes>
+              <Footer />
+        </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <label>Wellcome Home Yehoo</label>
+        </div>
+    )
+}
+
+export default Home
