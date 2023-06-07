c9331f4f47d7871d5bda35127ab86a267e716234
Tamir <tamir.spilberg@s.afeka.ac.il>
2023-06-07 19:28:57 +0300

    added docker

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,12 @@
  * Configuration object for the application.
  */
 const configuration = {
-    port: 8080,
-    uri: 'mongodb://127.0.0.1:27017/todoDB',
-    json_path: "./tests/utils/"
-  };
-  
-  export default configuration;
-  
+  isDocker: process.env.DOCKER === "true",
+  port: 8080,
+  uri: `mongodb://${
+    process.env.DOCKER === "true" ? "database" : "127.0.0.1"
+  }:27017/todoDB`,
+  json_path: "./tests/utils/",
+};
+
+export default configuration;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-import app from './app.js';
-import configurations from './config/config.js';
+import app from "./app.js";
+import configurations from "./config/config.js";
 
 /**
  * Retrieves the port from the configuration and starts the server.
  */
 const port = configurations.port;
+
+if (configurations.isDocker) {
+  console.log("Running in a Docker container");
+} else {
+  console.log("Run on User OS");
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
